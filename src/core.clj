7fcf73c8e5c63e49d41eba6d003201059f66d486
Carl Kamholtz <carl.kamholtz@gmail.com>
2022-05-01 17:07:46 +1000

    Move date parsing out of meta-csv + separate cols that are csv

--- src/core.clj
+++ src/core.clj
@@ -2,25 +2,15 @@
   (:require [nextjournal.clerk :as clerk]
             [dk.ative.docjure.spreadsheet :as ss]
             [meta-csv.core :as csv]
-            [java-time :as jt]))
-
-(defn load-first-sheet
-  "Return the first sheet of an Excel spreadsheet as a seq of maps."
-  [filename]
-  (let [rows (->> (ss/load-workbook filename) ; load the file
-                  (ss/sheet-seq)              ; seq of sheets in the file
-                  first                       ; take the first (only)
-                  ss/row-seq                  ; get the rows from it
-                  (drop 6)
-                  (mapv ss/cell-seq))         ; each row -> seq of cells
-        ;; break off the headers to produce a seq of maps
-        headers   (mapv (comp keyword ss/read-cell) (first rows))]
-    ;; map over the rows creating new maps with the headers as keys
-    (mapv #(zipmap headers (map ss/read-cell %)) (rest rows))))
+            [java-time :as jt]
+            [clojure.string :as str]))
 
 
 (def fields  [{:field :index, :type :long}
-              {:field :date, :type :string}
+              {:field :date, 
+               :type :string 
+               ; :preprocess-fn 
+               }
               {:field :lasted, :type :string}
               {:field :pain-level, :type :long}
               {:field :affected-activities, :type :string}
@@ -40,14 +30,64 @@
               {:field :notes, :type :string}])
 
 (comment 
+  (print last-date)
   ; Learning java-time
-  (jt/local-date-time "dd/MM/yy HH:mm" "26/12/21 06:26")
+  (jt/local-date-time "d/MM/yy HH:mm" "4/04/22 06:55")
+  (jt/local-date-time "d/MM/yy HH:mm" "31/01/22 07:42")
+  (jt/local-date-time "d/MM/yy HH:mm" "26/12/21 06:26")
+
+  (jt/local-date-time "d/MM/yy HH:mm" "2/03/22 06:54")
+  (jt/local-date-time "d/MM/yy HH:mm" "28/02/22 16:29")
+  (jt/local-date-time "d/MM/yy HH:mm" "26/02/22 07:49")
+  (jt/local-date-time "d/MM/yy HH:mm" "29/04/22 07:03")
+
+
   (jt/local-date-time "dd/MM/yy HH:mm" "21/12/21 22:00"))
+
+;; TODO: parse dates, 
+;; split :pain-positions (comma)
+;; split potential-triggers (comma)
+;; split :lasted (newline)
+;; split symptoms (comma)
+;;
 
 (def mb-path "./datasets/MigraineBuddy_20211216_20220430_1651315369524_-555206987.csv")
 
 (def mb-data
-  (csv/read-csv mb-path {:header? true :skip 6 :fields fields}))
+  (csv/read-csv mb-path {:header? false :skip 6 :fields fields}))
 
-(clerk/table mb-data)
+(defn parse-date-string [date-str]
+  (jt/local-date-time "d/MM/yy HH:mm" date-str))
+
+(defn parse-csv [csv-str]
+  (str/split csv-str #","))
+
+(comment 
+  (parse-csv "Right Front Head, Left Front Head, Left Eye, Right Eye")
+  (parse-csv nil)
+  )
+
+(defn parse-date [row]
+  (let [date-string (:date row)]
+    (assoc row :date (parse-date-string date-string))))
+
+(defn parse-col [col-key parse-fn]
+  (fn [row]
+    (let [str-in (col-key row)]
+      (assoc row col-key (parse-fn str-in)))))
+
+(def parsed-mb-data
+  (->> mb-data
+       (map (parse-col :date parse-date-string))
+       (map (parse-col :affected-activities parse-csv))
+       (map (parse-col :potential-triggers parse-csv))
+       (map (parse-col :symptoms parse-csv))
+       (map (parse-col :auras parse-csv))
+       ; (map (parse-col :helpful-medication parse-csv))
+       (map (parse-col :pain-positions parse-csv))
+
+       ))
+
+(clerk/table parsed-mb-data)
+
 
